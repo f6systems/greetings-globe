9fab58e5e9abce9d2aeb7ae448faa0d4811191d6
Douglas E. Hopley Jr <hopley@ipcloud.net>
2017-04-26 19:10:23 -0600

    dh - some initial see files +

---
+++ greetings-globe.go
@@ -0,0 +1,15 @@
+//filename=greetings-globe.go
+//see=hopley@f6systems.com
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Printf("init()\n")
+}
+
+func main() {
+	fmt.Printf("Starting\n")
+}
